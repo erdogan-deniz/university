30cfd9e10df5afc6aa5a1b1ae7b3e460efb89665
Deniz <erdogan33@mail.ru>
2024-11-10 01:56:40 +0300

    d

---
+++ Magistracy/математическое_и_компьютерное_моделирование/лабораторные_работы/лабораторная_работа_№_1/laboratory_work_one/__init__.py
@@ -0,0 +1,2 @@
+from data import solution_data
+from functions import lotka_volterra, runge_kutt_4th_order, draw_plots
---
+++ Magistracy/математическое_и_компьютерное_моделирование/лабораторные_работы/лабораторная_работа_№_1/main.py
@@ -0,0 +1,9 @@
+from laboratory_work_one import (
+    lotka_volterra,
+    draw_plots,
+    runge_kutt_4th_order,
+    solution_data,
+)
+
+if __name__ == "__name__":
+    ...
